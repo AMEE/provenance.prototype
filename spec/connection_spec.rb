77d651586f3e9058888ad3e91783b0c74eb1a034
James Hetherington <jamespjh@pyroclasm.(none)>
2010-05-24 12:07:16 +0100

    Initial skeleton checkin

---
+++ spec/connection_spec.rb
@@ -0,0 +1,15 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe Connection do
+  before(:each) do
+    @connection = Connection.new
+  end
+
+  it "should desc" do
+    # TODO
+  end
+end
+
