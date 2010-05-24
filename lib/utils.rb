e70e03e8085e8f721176eb7b17b3f2366e18e6c3
James Hetherington <jamespjh@pyroclasm.(none)>
2010-05-24 13:33:43 +0100

    Factor utils, log refinements

---
+++ lib/utils.rb
@@ -0,0 +1,18 @@
+require 'log4r'
+require 'log4r/yamlconfigurator'
+
+Home=File.join(File.dirname(File.dirname(__FILE__)))
+
+def configfile(mod)
+  File.join(Home,'config',"#{mod}.yml")
+end
+
+def config(mod)
+    YAML.load_file configfile(mod)
+end
+
+yc=Log4r::YamlConfigurator
+yc['HOME']=Home
+yc.load_yaml_file configfile('log')
+$log=Log4r::Logger['Main']
+$log.debug('Provenance started')
