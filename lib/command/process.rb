2188a595f083088a272c8e95df136005c01bf85e
James Hetherington <jamespjh@pyroclasm.(none)>
2010-05-24 23:32:45 +0100

    Capitalisation fix

--- lib/command/process.rb
+++ lib/command/process.rb
@@ -6,14 +6,14 @@ end
 
 prov :in do
   subject comment.newuri
-  type OPM.was_derived_from
+  type OPM.wasDerivedFrom
   qualify OPM.effect,comment.uri
   qualify OPM.cause,args.shift
 end
 
 prov :out do
   subject comment.newuri
-  type OPM.was_generated_by
+  type OPM.wasGeneratedBy
   qualify OPM.effect,args.shift
   qualify OPM.cause,comment.uri
 end
