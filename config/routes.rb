549748e5f1745852866f8fcc248b1617424afc8f
James Hetherington <jamespjh@pyroclasm.(none)>
2010-09-16 16:54:38 +0100

    Everything route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.connect 'everything/graph',:controller=>'account',:action=>'graph',:account=>'everything'
+  map.connect 'everything/report',:controller=>'account',:action=>'report',:account=>'everything'
   map.connect ':project/:issue/graph',:controller=>'account',:action=>'graph'
   map.connect ':project/:issue/report',:controller=>'account',:action=>'report'
   map.root :controller=>'account',:project=>'SC',:issue=>'47',:action=>'graph'
