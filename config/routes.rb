95fa8bca498fe5b137be0fdfbdcf7c803b80f5c5
James Hetherington <jamespjh@googlemail.com>
2010-11-26 13:54:49 +0000

    Add category paths to routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 ActionController::Routing::Routes.draw do |map|
-  map.connect 'everything/graph',:controller=>'account',:action=>'graph',:account=>'everything'
-  map.connect 'everything/report',:controller=>'account',:action=>'report',:account=>'everything'
+  map.connect 'everything/graph',:controller=>'account',:action=>'graph',:everything=>true
+  map.connect 'everything/report',:controller=>'account',:action=>'report',:everything=>true
   map.connect ':account/report',:controller=>'account',:action=>'report'
   map.connect ':account/graph',:controller=>'account',:action=>'graph'
-  map.root :controller=>'account',:project=>'SC',:issue=>'47',:action=>'graph'
+  map.connect 'category/*allpath/graph', :controller=>'account',:action=>'graph'
+  map.connect 'category/*allpath/report', :controller=>'account',:action=>'report'
+  map.connect 'textual/*textpath/graph', :controller=>'account',:action=>'graph'
+  map.connect 'textual/*textpath/report', :controller=>'account',:action=>'report'
+  map.connect 'issue/:project/:issue/graph', :controller=>'account',:action=>'graph'
+  map.connect 'issue/:project/:issue/report', :controller=>'account',:action=>'report'
+  map.root :controller=>'account',:allpath=>%w{transport car generic ghgp us},:action=>'graph'
 end
