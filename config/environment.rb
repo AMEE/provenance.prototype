04fde995a4b426f4bd35e52b46e811903e38db94
James Hetherington <jamespjh@googlemail.com>
2010-11-26 13:55:08 +0000

    Dependency on provenance gem

--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,7 @@ Rails::Initializer.run do |config|
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem "amee", :version => "~> 2.5.1"
   config.gem "my_amee_users", :version => '>= 1.0.0'
+  config.gem 'provenance', :lib => false, :version => '~>0.2.0'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
